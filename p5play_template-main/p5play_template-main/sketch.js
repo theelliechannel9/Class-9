4fa6e13f5da1676045cbe6ce058d26f511b0a345
theelliechannel9 <129701350+theelliechannel9@users.noreply.github.com>
2023-04-02 19:55:57 -0500

    Add files via upload

---
+++ p5play_template-main/p5play_template-main/sketch.js
@@ -0,0 +1,19 @@
+var box
+
+function setup() {
+  createCanvas(400,400);
+box=createSprite(200,200,20,20);
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW)){
+  box.position.x=box.position.x+5
+}
+  drawSprites();
+}
+
+
+
+
